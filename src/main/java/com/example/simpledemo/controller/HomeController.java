5395b9b3921583608ff57b031d2bb196332dfbf1
jarek <jaroslaw.zyzak@ailleron.com>
2019-06-17 13:14:03 +0200

    added a new method

--- src/main/java/com/example/simpledemo/controller/HomeController.java
+++ src/main/java/com/example/simpledemo/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.simpledemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping
-    public String home(){
-        return "Home";
+    public String home() {
+        return "Home!!!";
+    }
+
+    @GetMapping("/{username}")
+    public String checkUser(@PathVariable String username) {
+        String input = username;
+        if (input.equals("Jarek")) {
+            return "Witaj Jarek!!!";
+        } else {
+            return "Nie znam!!!";
+        }
     }
 }

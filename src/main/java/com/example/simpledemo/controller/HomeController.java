43774e0d4f83da9e6f8c785a5db331c9d93c39be
jarek <jaroslaw.zyzak@ailleron.com>
2019-06-18 10:44:05 +0200

    Revert "added a new method"
    This reverts commit 5395b9b3

--- src/main/java/com/example/simpledemo/controller/HomeController.java
+++ src/main/java/com/example/simpledemo/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.simpledemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,15 @@ public class HomeController {
     @GetMapping
     public String home(){
         return "Home";
+    }
+
+    @GetMapping("/{username}")
+    public String checkUser(@PathVariable String username) {
+        String input = username;
+        if (input.equals("Jarek")) {
+            return "Witaj Jarek!!!";
+        } else {
+            return "Nie znam!!!";
+        }
     }
 }

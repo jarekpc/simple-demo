fa688b47cea3a3e972e8c8dc68c2dcac954cab79
jarek <jaroslaw.zyzak@ailleron.com>
2019-06-19 15:14:18 +0200

    add testing to new branch

---
+++ src/main/java/com/example/simpledemo/controller/TestingController.java
@@ -0,0 +1,15 @@
+package com.example.simpledemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("testing")
+public class TestingController {
+
+    @GetMapping
+    public String testing(){
+        return "Testing!";
+    }
+}

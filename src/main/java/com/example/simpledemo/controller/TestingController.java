b914a9fd669d8f0f83480eccecc15ec81a39fc93
jarek <jaroslaw.zyzak@ailleron.com>
2019-06-19 15:37:15 +0200

    change name controller

--- src/main/java/com/example/simpledemo/controller/TestingController.java
+++ src/main/java/com/example/simpledemo/controller/TestingController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("testing")
+@RequestMapping("testing2")
 public class TestingController {
 
     @GetMapping
     public String testing(){
-        return "Testing!";
+        return "Testing!2";
     }
 }
